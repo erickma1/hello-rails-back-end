2adb87a745c2885544538dfe53d1bc2e04160195
erickma1 <110662611+erickma1@users.noreply.github.com>
2024-02-24 10:13:53 +0000

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,12 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Example:
+Message.create(content: "Hello, World!")
+Message.create(content: "Hi there!")
+Message.create(content: "Welcome!")
+Message.create(content: "Greetings!")
+Message.create(content: "Good day!")
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-Message.create(content: "Hello!")
-Message.create(content: "Hi there!")
-Message.create(content: "Greetings!")
-Message.create(content: "Welcome!")
-Message.create(content: "Salutations!")
